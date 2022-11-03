16855cdad3b55f39a9441430f26e2993b1d94233
Nguyen Le <nle244@csu.fullerton.edu>
2022-11-03 01:00:04 -0700

    just need FSA

--- parser.py
+++ parser.py
@@ -1,63 +1,51 @@
-input = 'while  (s < upper)   t = 33.00;'
-leximes = input.split()
 
-keyword = ['while', 'for', 'if', 'then', 'else']
-separator = '();'
-operator = '<='
-real = '1234567890.'
-result = []
+def lexer(lexemes):
+    keyword = ['while', 'for', 'if', 'then', 'else']
+    separator = '();'
+    operators = '<='
+    reals = '1234567890.'
+    result = []
+    id = ''
+    real = ''
 
-temp = list(real)
-for t in temp:
-    print(t)
-    print(t in real)
-for lexime in leximes:
-    if lexime in keyword:
-        result.append(('keyword',lexime))
-    else:
-        token_string = list(lexime)
-        id = ''
-        real = ''
-        for token in token_string:
-            if token in separator:
-                if len(id) > 0:
-                    result.append(('identifier',id))
-                    id = ''
-                if len(real) > 0:
-                    result.append(('real', real))
-                    real = ''
-                result.append(('separator', token))
-            if token in operator:
-                if len(id) > 0:
-                    result.append(('identifier',id))
-                    id = ''
-                if len(real) > 0:
-                    result.append(('real', real))
-                    real = ''
-                result.append(('operator', token))
-            if token in real:
-                real += token
-            elif token.isalpha():
-                id += token
+    for lexime in lexemes:
+        if lexime in keyword:
+            result.append(('keyword',lexime))
+        else:
+            token_string = list(lexime)
+            for token in token_string:
+                if token in reals:
+                    real += token
+                elif token.isalpha():
+                    id += token
+                elif token in operators:
+                    if len(id) > 0:
+                        result.append(('identifier',id))
+                        id = ''
+                    if len(real) > 0:
+                        result.append(('real', real))
+                        real = ''
+                    result.append(('operator', token))
 
-                
-        if len(id) > 0:
-            result.append(('identifier', id))
-        if len(real) > 0:
-            result.append(('real', real))
-        
+                elif token in separator:
+                    if len(id) > 0:
+                        result.append(('identifier',id))
+                        id = ''
+                    if len(real) > 0:
+                        result.append(('real', real))
+                        real = ''
+                    result.append(('separator', token))
+    return result
 
-        # for token in lexime:
-        #     token_string = token
-        #     id = []
-        #     real = []
-        #     if token in separator:
-        #         result.append(('separator',token))
-        #     elif token in operator:
-        #         result.append(('operator',token))
-        #     elif token.isalpha():
-        #         for t in token:
+with open('input_scode.tx') as f:
+    lines = [line for line in f]
 
+lex = lines[0].split()
 
-print(result)
+output = lexer(lex)
 
+with open('output.txt', 'w') as out:
+    out.write('{:<20s} {:<10s}'.format('tokens','lexemes\n'))
+    out.write('-----------------------------------------------\n')
+    for each in output:
+        out.write('{:<20s} {:<10s}\n'.format(each[0],each[1]))
